cb9c4871ef00e3faa430a6c41736280a5d1dbc26
Alyssa Cucunato <acucunato18@gmail.com>
2020-05-11 14:49:04 -0400

    added MAJJ

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,7 @@
 import React from "react";
 
 function Footer() {
-  return (
-    <div class="navbar-bottom sticky-footer">
-      <br />
-      alyssa cucunato © 2020
-    </div>
-  );
+  return <div class="navbar-bottom sticky-footer"></div>;
 }
 
 export default Footer;
