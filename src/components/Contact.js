ace491e0ab4cef3041ffd78a1bebdd7d795ef233
Alyssa Cucunato <acucunato18@gmail.com>
2020-06-18 17:34:16 -0400

    added email to contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,14 +7,34 @@ function Contact() {
       <div class="resume">
         <h1 class="pt-4 wow bounceInUp">let's get in touch</h1>
         <div class="horizontal-line"></div>
-        <a href={resume} download target="_blank" rel="noopener noreferrer">
-          <button
-            type="button"
-            class="btn btn-outline btn-lg wow tada delay-1s"
-          >
-            <i class="fas fa-download"></i> download my resume
-          </button>
-        </a>
+        <div class="wow fadeIn delay-1s">
+          I'd love to work with you! Feel free to reach out via email or
+          download my resume.
+        </div>
+        <br />
+
+        <div class="row">
+          <div class="col-lg-7">
+            <a href={resume} download target="_blank" rel="noopener noreferrer">
+              <button
+                type="button"
+                class="btn btn-outline btn-lg wow fadeIn delay-2s"
+              >
+                <i class="fas fa-download"></i> download my resume
+              </button>
+            </a>
+          </div>
+          <div class="col-lg-3">
+            <a href="mailto:acucunato18@gmail.com">
+              <button
+                type="button"
+                class="btn btn-outline btn-lg wow fadeIn delay-2s"
+              >
+                <i class="fa fa-envelope"></i> acucunato18@gmail.com
+              </button>
+            </a>
+          </div>
+        </div>
         <br />
         <br />
         <br />
